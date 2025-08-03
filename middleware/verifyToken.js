d3b8a0b30a12301af4e8479ea21795d5ee196d68
lal Hussain <159100097+Mr-Lal@users.noreply.github.com>
2025-08-03 01:44:57 -0400

    Update verifyToken.js
    change the token get code

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,15 +2,19 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   
-  let token = req.cookies?.token;
+  let token = req.cookies.token;
 
-  if (!token && req.headers.authorization?.startsWith("Bearer ")) {
-    token = req.headers.authorization.split(" ")[1]; 
-  }
+    if (!token) {
+      const authHeader = req.headers.authorization;
+      if (authHeader && authHeader.startsWith('Bearer ')) {
+        token = authHeader.split(' ')[1];
+      }
+    }
 
-  if (!token) {
-    return res.status(401).json({ msg: "No token provided" });
-  }
+    if (!token) {
+      return res.status(401).json({ msg: 'Unauthorized access, please login' });
+    }
+
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
